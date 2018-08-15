b778ff8001149649ec46cbb9b7343801d144d3a8
wutao <ldlywt@163.com>
2018-08-15 14:59:40 +0800

    ImageLoader更换为okhttp引擎

--- simpleimageloader/src/main/java/com/ldlywt/simpleimageloader/org/simple/imageloader/loader/UrlLoader.java
+++ simpleimageloader/src/main/java/com/ldlywt/simpleimageloader/org/simple/imageloader/loader/UrlLoader.java
@@ -33,37 +33,60 @@ import com.ldlywt.simpleimageloader.org.simple.imageloader.request.BitmapRequest
 
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * @author mrsimple
  */
 public class UrlLoader extends AbsLoader {
-
     @Override
-    public Bitmap onLoadImage(BitmapRequest request) {
-        final String imageUrl = request.imageUri;
-        FileOutputStream fos = null;
-        InputStream is = null;
-        Bitmap bitmap = null ;
-         HttpURLConnection conn = null ;
+    protected Bitmap onLoadImage(BitmapRequest result) {
         try {
-            URL url = new URL(imageUrl);
-            conn = (HttpURLConnection) url.openConnection();
-            is = new BufferedInputStream(conn.getInputStream());
-            bitmap =  BitmapFactory.decodeStream(is, null, null);
-        } catch (Exception e) {
-        	e.printStackTrace();
-        } finally {
-            IOUtil.closeQuietly(is);
-            IOUtil.closeQuietly(fos);
-            if ( conn != null ) {
-                // 关闭流
-                conn.disconnect();
-			}
+            String imageUri = result.imageUri;
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request.Builder().url(imageUri).build();
+            Response response = okHttpClient.newCall(request).execute();
+            InputStream inputStream = response.body().byteStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            return bitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return bitmap;
+        return null;
     }
+
+//    @Override
+//    public Bitmap onLoadImage(BitmapRequest request) {
+//        final String imageUrl = request.imageUri;
+//        FileOutputStream fos = null;
+//        InputStream is = null;
+//        Bitmap bitmap = null ;
+//         HttpURLConnection conn = null ;
+//        try {
+//            URL url = new URL(imageUrl);
+//            conn = (HttpURLConnection) url.openConnection();
+//            is = new BufferedInputStream(conn.getInputStream());
+//            bitmap =  BitmapFactory.decodeStream(is, null, null);
+//        } catch (Exception e) {
+//        	e.printStackTrace();
+//        } finally {
+//            IOUtil.closeQuietly(is);
+//            IOUtil.closeQuietly(fos);
+//            if ( conn != null ) {
+//                // 关闭流
+//                conn.disconnect();
+//			}
+//        }
+//        return bitmap;
+//    }
+
+
+
 }
