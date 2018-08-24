69b71862bac479478732858ea438fa4733821535
wutao <ldlywt@163.com>
2018-08-24 15:39:15 +0800

    ImageFragment切换为瀑布流的recycleview

--- app/src/main/java/com/ldlywt/androidadvancedemo/ui/fragment/ImagesFragment.java
+++ app/src/main/java/com/ldlywt/androidadvancedemo/ui/fragment/ImagesFragment.java
@@ -1,24 +1,50 @@
-
 package com.ldlywt.androidadvancedemo.ui.fragment;
 
-import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.google.gson.Gson;
+import com.ldlywt.androidadvancedemo.App;
 import com.ldlywt.androidadvancedemo.R;
+import com.ldlywt.androidadvancedemo.bean.PictureBean;
 import com.ldlywt.base.fragment.BaseFragment;
+import com.ldlywt.easyhttp.Call;
+import com.ldlywt.easyhttp.Callback;
+import com.ldlywt.easyhttp.HttpClient;
+import com.ldlywt.easyhttp.Request;
+import com.ldlywt.easyhttp.Response;
 import com.ldlywt.simpleimageloader.org.simple.imageloader.cache.DoubleCache;
 import com.ldlywt.simpleimageloader.org.simple.imageloader.config.ImageLoaderConfig;
 import com.ldlywt.simpleimageloader.org.simple.imageloader.core.SimpleImageLoader;
 import com.ldlywt.simpleimageloader.org.simple.imageloader.policy.ReversePolicy;
+import com.orhanobut.logger.Logger;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * <pre>
+ *     author : lex
+ *     e-mail : ldlywt@163.com
+ *     time   : 2018/08/24
+ *     desc   :  * 图片加载Fragment，使用手写的http和手写的Imageload
+ *     version: 1.0
+ * </pre>
+ */
 public class ImagesFragment extends BaseFragment {
+
+    private static final String URL = "http://image.baidu.com/channel/listjson?pn=0&rn=30&tag1=美女&tag2=全部&ie=utf8";
+    private ImageItemAdapter mAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,14 +53,44 @@ public class ImagesFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.gridview_fragment, container,
                 false);
-        initImageLoader();
-        GridView gridView = (GridView) view.findViewById(R.id.id_gridview);
-        gridView.setAdapter(new ImageItemAdaper(getActivity(), 0,
-                imageThumbUrls));
+        RecyclerView recyclerView = view.findViewById(R.id.recycleview);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        recyclerView.addItemDecoration(new DividerItemDecoration(App.getApp(), DividerItemDecoration.VERTICAL));
+        mAdapter = new ImageItemAdapter();
+        recyclerView.setAdapter(mAdapter);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initImageLoader();
+        HttpClient httpClient = new HttpClient();
+        Request request = new Request
+                .Builder()
+                .url(URL)
+                .build();
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Logger.i(response.getBody());
+                Gson gson = new Gson();
+                PictureBean pictureBean = gson.fromJson(response.getBody(), PictureBean.class);
+                getActivity().runOnUiThread(() -> {
+                    mAdapter.setList(pictureBean.data);
+                    mAdapter.getRandomHeight(pictureBean.data);
+                    mAdapter.notifyDataSetChanged();
+                });
+            }
+        });
     }
 
     @Override
@@ -54,123 +110,57 @@ public class ImagesFragment extends BaseFragment {
         SimpleImageLoader.getInstance().init(config);
     }
 
+    public static class ImageViewHolder extends RecyclerView.ViewHolder {
 
-    /**
-     * @author mrsimple
-     */
-    private class ImageItemAdaper extends ArrayAdapter<String> {
+        private final ImageView mImageView;
 
-        public ImageItemAdaper(Context context, int resource, String[] datas) {
-            super(getActivity(), 0, datas);
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.img);
+        }
+    }
+
+    private class ImageItemAdapter extends RecyclerView.Adapter<ImageViewHolder> {
+
+        private List<PictureBean.DataBean> list = new ArrayList<>();
+        private List<Integer> mHeights;
+
+        public ImageItemAdapter() {
+        }
+
+        public void setList(List<PictureBean.DataBean> list) {
+            this.list = list;
+        }
+
+        public void getRandomHeight(List<PictureBean.DataBean> mList) {
+            mHeights = new ArrayList<>();
+            for (int i = 0; i < mList.size(); i++) {
+                //随机的获取一个范围为200-600直接的高度
+                mHeights.add((int) (300 + Math.random() * 400));
+            }
+        }
+
+        @NonNull
+        @Override
+        public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.image_item_layout, parent, false);
+            return new ImageViewHolder(view);
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = getActivity().getLayoutInflater().inflate(
-                        R.layout.image_item_layout, parent, false);
-            }
-            ImageView imageview = (ImageView) convertView
-                    .findViewById(R.id.id_img);
+        public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
             // 加载图片
-            SimpleImageLoader.getInstance().displayImage(imageview,
-                    getItem(position));
-            return convertView;
+            ViewGroup.LayoutParams layoutParams = holder.itemView.getLayoutParams();
+            layoutParams.height = mHeights.get(position);
+            holder.itemView.setLayoutParams(layoutParams);
+            SimpleImageLoader.getInstance().displayImage(holder.mImageView,
+                    list.get(position).image_url);
         }
 
-    } // end ImageItemAdaper
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+    }
 
-
-    public final static String[] imageThumbUrls = new String[] {
-            "http://images.newsdog.today/thumb_e2f6cea2ba6ad2035ddce3784d45df74",
-            "http://img.my.csdn.net/uploads/201407/26/1406383291_6518.jpg",
-            "http://images.newsdog.today/origin_5b86722b2de1e6996abd5770b1c343a8",
-            "http://images.newsdog.today/thumb_fa06abd117c930d7199c148ee339c766",
-            "http://img.my.csdn.net/uploads/201407/26/1406383290_1042.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383275_3977.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383265_8550.jpg",
-            // 以下三张为本地图片,本地图片支持uri格式，形如file:// + 图片绝对路径
-            "file:///storage/emulated/0/Camera/P41115-140216.jpg",
-            "file:///storage/emulated/0/Camera/P41115-142950.jpg",
-            "file:///storage/emulated/0/Camera/P50102-133614.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/not_found_haha.jpg", // 不存在的图片
-            "http://img.my.csdn.net/uploads/201407/26/1406383264_3954.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383264_4787.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383264_8243.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383248_3693.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383243_5120.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383242_3127.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383242_9576.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383242_1721.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383219_5806.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383214_7794.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383213_4418.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383213_3557.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383210_8779.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383172_4577.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383166_3407.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383166_2224.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383166_7301.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383165_7197.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383150_8410.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383131_3736.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383130_5094.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383130_7393.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383129_8813.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383100_3554.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383093_7894.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383092_2432.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383092_3071.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383091_3119.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383059_6589.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383059_8814.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383059_2237.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383058_4330.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406383038_3602.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382942_3079.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382942_8125.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382942_4881.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382941_4559.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382941_3845.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382924_8955.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382923_2141.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382923_8437.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382922_6166.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382922_4843.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382905_5804.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382904_3362.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382904_2312.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382904_4960.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382900_2418.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382881_4490.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382881_5935.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382880_3865.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382880_4662.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382879_2553.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382862_5375.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382862_1748.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382861_7618.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382861_8606.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382861_8949.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382841_9821.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382840_6603.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382840_2405.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382840_6354.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382839_5779.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382810_7578.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382810_2436.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382809_3883.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382809_6269.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382808_4179.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382790_8326.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382789_7174.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382789_5170.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382789_4118.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382788_9532.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382767_3184.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382767_4772.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382766_4924.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382766_5762.jpg",
-            "http://img.my.csdn.net/uploads/201407/26/1406382765_7341.jpg"
-    };
 }
