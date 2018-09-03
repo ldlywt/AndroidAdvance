1597cfac7743c1eeacc833ebf4166cb98c256a8f
wutao <ldlywt@163.com>
2018-09-03 16:36:10 +0800

    去掉dagger

--- app/src/main/java/com/ldlywt/androidadvancedemo/dagger/DiscoverFragmentComponent.java
+++ app/src/main/java/com/ldlywt/androidadvancedemo/dagger/DiscoverFragmentComponent.java
@@ -1,19 +1,19 @@
-package com.ldlywt.androidadvancedemo.dagger;
-
-import com.ldlywt.androidadvancedemo.ui.fragment.maintab.DiscoveryFragment;
-
-import dagger.Component;
-
-/**
- * <pre>
- *     author : wutao
- *     e-mail : ldlywt@163.com
- *     time   : 2018/09/02
- *     desc   :
- *     version: 1.0
- * </pre>
- */
-@Component(modules = DiscoveryFragmentModule.class)
-public interface DiscoverFragmentComponent {
-    void inject(DiscoveryFragment discoveryFragment);
-}
+//package com.ldlywt.androidadvancedemo.dagger;
+//
+//import com.ldlywt.androidadvancedemo.ui.fragment.maintab.DiscoveryFragment;
+//
+//import dagger.Component;
+//
+///**
+// * <pre>
+// *     author : wutao
+// *     e-mail : ldlywt@163.com
+// *     time   : 2018/09/02
+// *     desc   :
+// *     version: 1.0
+// * </pre>
+// */
+//@Component(modules = DiscoveryFragmentModule.class)
+//public interface DiscoverFragmentComponent {
+//    void inject(DiscoveryFragment discoveryFragment);
+//}
--- app/src/main/java/com/ldlywt/androidadvancedemo/ui/fragment/maintab/DiscoveryFragment.java
+++ app/src/main/java/com/ldlywt/androidadvancedemo/ui/fragment/maintab/DiscoveryFragment.java
@@ -1,22 +1,8 @@
 package com.ldlywt.androidadvancedemo.ui.fragment.maintab;
 
 import android.os.Bundle;
-
-import com.ldlywt.androidadvancedemo.Constant;
 import com.ldlywt.androidadvancedemo.R;
-import com.ldlywt.androidadvancedemo.dagger.DaggerDiscoverFragmentComponent;
 import com.ldlywt.base.view.BaseFragment;
-import com.orhanobut.logger.Logger;
-
-import java.io.IOException;
-
-import javax.inject.Inject;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * <pre>
@@ -29,8 +15,8 @@ import okhttp3.Response;
  */
 public class DiscoveryFragment extends BaseFragment{
 
-    @Inject
-    OkHttpClient mOkHttpClient;
+//    @Inject
+//    OkHttpClient mOkHttpClient;
 
 
     public static DiscoveryFragment newInstance(String from) {
@@ -48,19 +34,19 @@ public class DiscoveryFragment extends BaseFragment{
 
     @Override
     public void initData(Bundle savedInstanceState) {
-        DaggerDiscoverFragmentComponent.create().inject(this);
-        Request request = new Request.Builder().url(Constant.URL_WEATHER_GET).build();
-        mOkHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Logger.i(response.message());
-            }
-        });
+//        DaggerDiscoverFragmentComponent.create().inject(this);
+//        Request request = new Request.Builder().url(Constant.URL_WEATHER_GET).build();
+//        mOkHttpClient.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                Logger.i(response.message());
+//            }
+//        });
     }
 
     @Override
