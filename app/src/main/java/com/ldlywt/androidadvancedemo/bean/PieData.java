6880a16c5ba161aa59380ff908b8c96e9f1b5fbe
wutao <670831931@qq.com>
2018-11-15 19:15:47 +0800

    实现扇饼PieView

---
+++ app/src/main/java/com/ldlywt/androidadvancedemo/bean/PieData.java
@@ -0,0 +1,63 @@
+package com.ldlywt.androidadvancedemo.bean;
+
+/**
+ * <pre>
+ *     author : wutao
+ *     time   : 2018/11/15
+ *     desc   :
+ * </pre>
+ */
+public class PieData {
+
+    private String name;
+    private float value;
+    private float percentage;
+
+    private int color = 0;
+    private float angle = 0;
+
+    public PieData(String name, float value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name == null ? "" : name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+}
--- app/src/main/java/com/ldlywt/androidadvancedemo/ui/fragment/maintab/DiscoveryFragment.java
+++ app/src/main/java/com/ldlywt/androidadvancedemo/ui/fragment/maintab/DiscoveryFragment.java
@@ -1,20 +1,15 @@
 package com.ldlywt.androidadvancedemo.ui.fragment.maintab;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-import android.widget.ImageView;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.ldlywt.androidadvancedemo.R;
+import com.ldlywt.androidadvancedemo.bean.PieData;
+import com.ldlywt.androidadvancedemo.view.PieView;
 import com.ldlywt.base.view.BaseFragment;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 
 /**
  * <pre>
@@ -25,11 +20,10 @@ import java.io.InputStream;
  *     version: 1.0
  * </pre>
  */
-public class DiscoveryFragment extends BaseFragment implements SurfaceHolder.Callback, View.OnClickListener {
+public class DiscoveryFragment extends BaseFragment implements View.OnClickListener {
 
-    private ImageView mImageView;
-    private SurfaceView mSurfaceView;
-    private android.hardware.Camera mCamera;
+
+    private PieView mPieView;
 
     public static DiscoveryFragment newInstance(String from) {
         DiscoveryFragment fragment = new DiscoveryFragment();
@@ -46,83 +40,25 @@ public class DiscoveryFragment extends BaseFragment implements SurfaceHolder.Cal
 
     @Override
     public void initData(Bundle savedInstanceState) {
-        mCamera = android.hardware.Camera.open();
-        mCamera.setDisplayOrientation(90);
-    }
-
-    private void loadImage2() {
-        mSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                if (holder == null) {
-                    return;
-                }
-                Paint paint = new Paint();
-                paint.setAntiAlias(true);
-                paint.setStyle(Paint.Style.STROKE);
-                InputStream inputStream = null;
-                try {
-                    inputStream = getActivity().getAssets().open("11.png");
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                    Canvas canvas = holder.lockCanvas();
-                    canvas.drawBitmap(bitmap, 0, 0, paint);
-                    holder.unlockCanvasAndPost(canvas);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-
-            }
-        });
-    }
-
-    private void loadImage1() {
-        try {
-            InputStream inputStream = getActivity().getAssets().open("11.png");
-            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-            mImageView.setImageBitmap(bitmap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ArrayList<PieData> list = new ArrayList<>();
+        list.add(new PieData("1", 20));
+        list.add(new PieData("2", 10));
+        list.add(new PieData("3", 50));
+        list.add(new PieData("4", 40));
+        list.add(new PieData("5", 35));
+        mPieView.setData(list);
+        mPieView.setStartAngle(0);
     }
 
     @Override
     public void initView() {
-        mImageView = getView().findViewById(R.id.iv);
-        mSurfaceView = getView().findViewById(R.id.surfaceview);
-        getView().findViewById(R.id.bt1).setOnClickListener(this);
-//        loadImage1();
-//        loadImage2();
-        mSurfaceView.getHolder().addCallback(this);
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        try {
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        mCamera.release();
+        mPieView = getView().findViewById(R.id.pie);
+        mPieView.setOnPieListener(new PieView.PieListener() {
+            @Override
+            public void onItemClick(float value) {
+                ToastUtils.showShort("点击的value： " + value);
+            }
+        });
     }
 
     @Override
