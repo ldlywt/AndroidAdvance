f4f912ccec0ec28c922903329207192bf0708536
lex <wutao@hey900.com>
2020-02-29 20:05:12 +0800

    HOOK Context 的 Activity 启动

--- app/src/main/java/com/ldlywt/androidadvancedemo/hook/InstrumentationProxy.java
+++ app/src/main/java/com/ldlywt/androidadvancedemo/hook/InstrumentationProxy.java
@@ -42,6 +42,9 @@ public class InstrumentationProxy extends Instrumentation {
         return null;
     }
 
+    /**
+     * HOOK Activity 的 Activity 启动
+     */
     public static void replaceActivityInstrumentation(Activity activity) {
         try {
             Field field = Activity.class.getDeclaredField("mInstrumentation");
@@ -52,7 +55,27 @@ public class InstrumentationProxy extends Instrumentation {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    /**
+     * HOOK Context 的 Activity 启动
+     */
+    public static void attachContext() throws Exception {
+        // 先获取到当前的ActivityThread对象
+        Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+        Method currentActivityThreadMethod = activityThreadClass.getDeclaredMethod("currentActivityThread");
+        currentActivityThreadMethod.setAccessible(true);
+        //currentActivityThread是一个static函数所以可以直接invoke，不需要带实例参数
+        Object currentActivityThread = currentActivityThreadMethod.invoke(null);
+
+        // 拿到原始的 mInstrumentation字段
+        Field mInstrumentationField = activityThreadClass.getDeclaredField("mInstrumentation");
+        mInstrumentationField.setAccessible(true);
+        Instrumentation mInstrumentation = (Instrumentation) mInstrumentationField.get(currentActivityThread);
+        // 创建代理对象
+        Instrumentation evilInstrumentation = new InstrumentationProxy(mInstrumentation);
+        // 偷梁换柱
+        mInstrumentationField.set(currentActivityThread, evilInstrumentation);
     }
 
 }
